b3a173db343d034bd01af4d0e2944e3c382d3383
Nico Lerchl <nico@lerchl.one>
2023-06-21 16:55:13 +0200

    spring boot project

---
+++ src/main/java/xyz/yustheyokai/sharexspringbootcustomuploader/SharexSpringBootCustomUploaderApplication.java
@@ -0,0 +1,13 @@
+package xyz.yustheyokai.sharexspringbootcustomuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharexSpringBootCustomUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharexSpringBootCustomUploaderApplication.class, args);
+	}
+
+}
